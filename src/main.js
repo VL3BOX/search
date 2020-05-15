f66975ebbbe4b74586fb81c8cc68ae1d14330d1d
iRuxu <rx6@qq.com>
2020-05-15 20:12:17 +0800

    sync common

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
-// import Vue from "vue";
-// import ElementUI from "element-ui";
-// import "element-ui/lib/theme-chalk/index.css";
-// Vue.use(ElementUI);
-
+// 第三方UI组件
+import Vue from "vue";
 Vue.config.productionTip = false;
-const JX3BOX = require("@jx3box/jx3box-common/js/jx3box");
+import ElementUI from "element-ui";
+Vue.use(ElementUI);
 
+// 通用UI模块
+// import JX3BOX_UI from '@jx3box/jx3box-common-ui'
+import "@jx3box/jx3box-common/css/element.css";
+// Vue.use(JX3BOX_UI);
+
+const JX3BOX = require("@jx3box/jx3box-common/js/jx3box");
 import store from "./store";
 import App from "./App.vue";
 new Vue({
